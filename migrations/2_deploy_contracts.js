67c244cbb93157b02b028185235508fc03d9762d
nutchanonc <nutchanonc@github.com>
2021-10-06 02:53:30 +0700

    used for test on 5 Dec 2021

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,7 +3,7 @@ const WETH = artifacts.require("WETH.sol")
 
 module.exports = async function (deployer) {
     let weth;
-    const FACTORY_ADDRESS = '0x1e17A2355caea4499b1936E4C60069435905B6dF';         // factory address here
+    const FACTORY_ADDRESS = '0xa03FfEA0C179EA31b461Aa8f6c2E0aC2d712E381';         // factory address here
 
 
     // FOR TESTNET DEPLOYMENT
