6eab3074495a6f88d92645aa1a0d01f8b7ae954a
nutchanonc <nutchanonc@github.com>
2021-10-06 23:20:02 +0700

    change deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,16 +3,16 @@ const WETH = artifacts.require("WETH.sol")
 
 module.exports = async function (deployer) {
     let weth;
-    const FACTORY_ADDRESS = '0xa03FfEA0C179EA31b461Aa8f6c2E0aC2d712E381';         // factory address here
+    const FACTORY_ADDRESS = '0x545aff39B979acD12703D30E3758D8269CF7493F'; // factory address here
 
 
     // FOR TESTNET DEPLOYMENT
-    // weth = await WETH.at('')            // use WBNB instead
+    weth = await WETH.at('0x094616f0bdfb0b526bd735bf66eca0ad254ca81f')            // use WBNB instead
 
 
     // FOR LOCAL BLOCKCHAIN DEPLOYMENT
-    await deployer.deploy(WETH);
-    weth = await WETH.deployed();
+    // await deployer.deploy(WETH);
+    // weth = await WETH.deployed();
 
 
     await deployer.deploy(Router, FACTORY_ADDRESS, weth.address);
